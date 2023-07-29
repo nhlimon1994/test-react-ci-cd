5a9c1a2855314af6c04e738425a2c3027ff32b93
jatrinahidhasanlimon <97885678+jatrinahidhasanlimon@users.noreply.github.com>
2023-07-29 17:58:48 +0600

    its ok

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Allright
+          Allright From Sheehan Rahman!!!!!!!!!!!!!!!
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Its ok Limon
+          Its ok Limon!!!
         </a>
       </header>
     </div>
